69268b10b0951e564430625750111ff1d5351475
VarshaAnoop <varsharose2014@gmail.com>
2022-11-24 16:20:44 +0530

    bank

---
+++ bankproject/bankproject/bankapp/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .import views
+from django.urls import path
+
+urlpatterns = [
+
+    path('',views.demo,name='demo'),
+    # path('demo1/',views.demo1,name='demo1')
+
+]
